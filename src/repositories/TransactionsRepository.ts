c336158a9b833005b4c0ae57c42f6cb4ae78462a
Rômulo Rocha <romulorocha063@gmail.com>
2020-05-02 02:29:17 -0300

    finish

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acumulator, currentValuel) => {
+      return currentValuel.type === 'income'
+        ? acumulator + currentValuel.value
+        : acumulator;
+    }, 0);
+
+    const outcome = this.transactions.reduce((acumulator, currentValuel) => {
+      return currentValuel.type === 'outcome'
+        ? acumulator + currentValuel.value
+        : acumulator;
+    }, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
